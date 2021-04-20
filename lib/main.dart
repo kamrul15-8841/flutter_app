3d4b767aa6075d2313067a9ccb1c556984d6fc5b
Md. Kamrul Hasan <kamrul15-8841@diu.edu.bd>
2021-04-21 00:50:11 +0600

    before stateless

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,46 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-      home: Text('Kamrul'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+              'My Application',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.white,
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.red[700],
+        ),
+            body: Center(
+              child: Text(
+                  'This is my Application',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40.0,
+                  color: Colors.red[700],
+                ),
+              ),
+            ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text('Click'),
+          backgroundColor: Colors.red[700],
+        ),
+
+
+      ),
   ));
+  
+  class Home extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+      return Container();
+    }
+  }
+  
 
 }
